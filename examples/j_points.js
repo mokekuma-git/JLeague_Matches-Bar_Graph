ab310f5a1aca10d55eb2aacbbb50abf2963a1b13
mokekuma-git <mokekuma.git@gmail.com>
2021-05-30 01:40:12 +0900

    Render bar graph dynamically by javascript

--- examples/j_points.js
+++ examples/j_points.js
@@ -1,16 +1,131 @@
-var space_color;
+// TODO: Global変数以外の解決方法は、後で調べる
+var height_unit;
+// var inputs; .... PythonでHTML内に定義
+
+const CATEGORY_TOP_TEAMS = [3, 2, 2]
+const CATEGORY_BOTTOM_TEAMS = [4, 4, 0]
+const CATEGORY_TEAMS_COUNT = [20, 22, 15]
 
 window.addEventListener("load", init, false);
 
 function init() {
-  space_color = document.querySelector("#space");
-  space_color.addEventListener("change", updateSpace, false);
+  document.querySelector("#space").addEventListener("change", updateSpace, false);
+  height_unit = parseInt(window.getComputedStyle(document.querySelector('.short')).getPropertyValue('height'));
+  document.querySelector("#team_sort_key").addEventListener("change", render_bar_graph, false);
+  document.querySelector("#old_bottom").addEventListener("change", render_bar_graph, false);
+  render_bar_graph();
 }
 
+
+function make_insert_columns(category) {
+    //各カテゴリの勝ち点列を入れる敷居位置を決定
+    //    昇格チーム (ACL出場チーム)、中間、降格チームの位置に挟む
+    category -= 1;
+    var columns = [CATEGORY_TOP_TEAMS[category], Math.floor(CATEGORY_TEAMS_COUNT[category] / 2)];
+    if(CATEGORY_BOTTOM_TEAMS[category])
+        columns.push(CATEGORY_TEAMS_COUNT[category] - CATEGORY_BOTTOM_TEAMS[category]);
+    return columns;
+}
+
+
+function make_html_column(target_team, team_data, max_point) {
+    //抽出したチームごとのDataFrameを使って、HTMLでチームの勝ち点積み上げ表を作る
+    //    target_team: 対象チームの名称
+    //    team_data:
+    //        .df: make_team_dfで抽出したチームデータ (試合リスト)
+    //        .point: 対象チームの最大勝ち点
+    //    max_point: 全チーム中の最大勝ち点
+    team_name = '<div class="short box ' + target_team + '">' + target_team + '</div>\n';
+    var box_list = [];
+    team_data.df.sort().forEach(function(_row) {
+        if(! _row['has_result']) {
+            box_height = 3;
+            future = true;
+        } else {
+            box_height = _row['point'];
+            future = false;
+        }
+        if(box_height == 0)
+            return;
+
+        if(_row['match_date'] instanceof String) {
+            match_date = _row['match_date'];
+        } else {
+            match_date = (_row['match_date']) ? _row['match_date'] : '未定 ';
+        }
+
+        if(box_height == 3) {
+            content = match_date + _row['opponent'] + '<br/>';
+            content += _row['goal_get'] + '-' + _row['goal_lose'] + '<br/>' + _row['stadium'] + '<br/>';
+            if(future) {
+                box_html = '<div class="tall box"><div class="future bg ' + target_team + '"></div><p>' + content + '</p></div>\n';
+            } else {
+                box_html = '<div class="tall box"><p class="' + target_team + '">' + content + '</p></div>\n';
+            }
+        } else {
+            box_html = '<div class="short box"><p class="' + target_team + '">' + match_date + _row['opponent'] + '</p></div>\n';
+            // _row['goal_get'] + '-' + _row['goal_lose'] + '<br/>';
+        }
+        box_list.push(box_html);
+    });
+
+    space_cols = max_point - team_data.avlbl_pt;
+    // console.log(target_team, space_cols)
+    if(space_cols) {
+        box_list.push('<div class="space box" style="height:' + height_unit * space_cols + 'px">(' + space_cols + ')</div>');
+    }
+
+    if(document.querySelector('#old_bottom').value == 'true') {
+        box_list.reverse();
+    }
+    return '<div>' + team_name + box_list.join('') + team_name + '</div>\n\n';
+}
+
+
+function make_point_column(max_point) {
+    // 勝点列を作って返す
+    var box_list = []
+    Array.from(Array(max_point), (v, k) => k + 1).forEach(function(_i) {
+        box_list.push('<div class="point box">' + _i + '</div>')
+    });
+    if(document.querySelector('#old_bottom').value == 'true') {
+        box_list.reverse();
+    }
+    return '<div><div class="point box">勝点</div>' + box_list.join('') + '<div class="point box">勝点</div></div>\n\n'
+}
+
+
+function render_bar_graph() {
+    var boxContainer = document.querySelector('.boxContainer');
+    boxContainer.innerHTML = '';
+    var columns = {};
+    Object.keys(inputs.matches).forEach(function(key){
+        columns[key] = make_html_column(key, inputs.matches[key], inputs.max_point);
+    });
+    insert_point_columns = make_insert_columns(inputs.category);
+    point_column = make_point_column(inputs.max_point);
+
+    boxContainer.innerHTML += point_column;
+    get_sorted_team_list(inputs.matches).forEach(function(key, index) {
+        if(insert_point_columns.includes(index))
+            boxContainer.innerHTML += point_column;
+        boxContainer.innerHTML += columns[key];
+    });
+    boxContainer.innerHTML += point_column;
+
+}
+
+
+function get_sorted_team_list(matches) {
+    var sort_key = document.querySelector('#team_sort_key').value;
+    return Object.keys(matches).sort(function(a, b) {return matches[b][sort_key] - matches[a][sort_key]});
+}
+
+/////////////////////////////////////////////////////////////// 背景調整用
 function updateSpace(event) {
   document.querySelectorAll(".space").forEach(function(space) {
     space.style.backgroundColor = event.target.value;
-    space.style.color = getBright(event.target.value, mod) > 0.5 ? 'black' : 'white'
+    space.style.color = getBright(event.target.value, mod) > 0.5 ? 'black' : 'white';
   });
 }
 
---
+++ j_points.js
@@ -0,0 +1,1 @@
+examples/j_points.js
