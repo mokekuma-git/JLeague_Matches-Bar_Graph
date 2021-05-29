f2b8a5e8b622750c920450cd6ebda4fc787d8b79
mokekuma-git <mokekuma.git@gmail.com>
2021-05-29 14:58:39 +0900

    refactoring according to pylint & read_jleague_matches.py can accept multiple category as '1-3'

--- read_jleague_matches.py
+++ read_jleague_matches.py
@@ -11,8 +11,6 @@ DEBUG = False
 # Jリーグ公開の各節試合情報のURL
 SOURCE_URL_FORMAT = 'https://www.jleague.jp/match/section/j{}/{}/'
 
-COL_NAMES = ['match_date', 'section_no', 'match_index_in_section', 'start_time', 'stadium',
-             'home_team', 'home_goal', 'away_goal', 'away_team']
 
 def read_match_data(soup: BeautifulSoup):
     """Jリーグの各節の試合情報リストから内容を読み込んで返す
@@ -32,22 +30,23 @@ def read_match_data(soup: BeautifulSoup):
         section_no = re.search('第(.+)節', section_no)[1]
         #print((match_date, section_no))
         for _tr in _section.find_all('tr'):
+            match_dict = {}
+            match_dict['match_date'] = match_date
+            match_dict['section_no'] = section_no
+            match_dict['match_index_in_section'] = i
             stadium_td = _tr.find('td', class_='stadium')
             if not stadium_td:
                 continue
-            start_time = re.search('([^\>]+)\<br', str(stadium_td))[1]
-            stadium = re.search('([^\>]+)\<\/a', str(stadium_td))[1]
-            home_team = _tr.find('td', class_='clubName rightside').text.strip()
-            home_goal = _tr.find('td', class_='point rightside').text.strip()
-            away_team = _tr.find('td', class_='clubName leftside').text.strip()
-            away_goal = _tr.find('td', class_='point leftside').text.strip()
-            _str_match_date = (match_date.strftime("%Y/%m/%d") if match_date else '未定')
+            match_dict['start_time'] = re.search(r'([^\>]+)\<br', str(stadium_td))[1]
+            match_dict['stadium'] = re.search(r'([^\>]+)\<\/a', str(stadium_td))[1]
+            match_dict['home_team'] = _tr.find('td', class_='clubName rightside').text.strip()
+            match_dict['home_goal'] = _tr.find('td', class_='point rightside').text.strip()
+            match_dict['away_goal'] = _tr.find('td', class_='point leftside').text.strip()
+            match_dict['away_team'] = _tr.find('td', class_='clubName leftside').text.strip()
+            # str_match_date = (match_date.strftime("%Y/%m/%d") if match_date else '未定')
             if DEBUG:
-                print(f'{_str_match_date} {section_no}節 {i} {start_time} [{stadium}]' + \
-                      f' {home_team} ({home_goal}) - ({away_goal}) {away_team}')
-            # 追加内容は、COL_NAMES に合わせること (追加する時は、必ず同時に更新)
-            result_list.append((match_date, section_no, i, start_time, stadium,
-                                home_team, home_goal, away_goal, away_team))
+                print(match_dict)
+            result_list.append(match_dict)
             i += 1
     return result_list
 
@@ -60,12 +59,25 @@ def read_all_matches(category: int):
     for _i in range(1, match_counts[category]):
         soup = BeautifulSoup(requests.get(SOURCE_URL_FORMAT.format(category, _i)).text, 'lxml')
         result_list = read_match_data(soup)
-        all_matches = pd.concat([all_matches, pd.DataFrame(result_list, columns=COL_NAMES)])
+        all_matches = pd.concat([all_matches, pd.DataFrame(result_list)])
+    all_matches.reset_index(drop=True)
     return all_matches
+
+
+def parse_argv(arg: str):
+    """引数をパースする
+        数値と、"数値-数値" の形式を受け取り、範囲全体の数値リストに変換
+        1-3 -> [1, 2, 3]
+    """
+    match = re.match(r'(\d)\-(\d)', arg)
+    if match:
+        return list(range(int(match[1]), int(match[2]) + 1))
+    return [int(arg)]
 
 
 if __name__ == '__main__':
     import sys
-    category = int(sys.argv[1])
-    all_matches = read_all_matches(category)
-    all_matches.to_csv(f'match_result-J{category}-{datetime.now().strftime("%Y%m%d")}.csv')
+    for _category in parse_argv(sys.argv[1]):
+        print(f'Start read J{_category} matches...')
+        _DF = read_all_matches(_category)
+        _DF.to_csv(f'match_result-J{_category}-{datetime.now().strftime("%Y%m%d")}.csv')
