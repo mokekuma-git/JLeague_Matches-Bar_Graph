447cc9b59bbd1029a3cd86b9a01538d6d21f44d3
mokekuma-git <mokekuma.git@gmail.com>
2021-07-07 19:02:44 +0900

    Read match_card_id from JLeague data site.

--- src/read_older2020_matches.py
+++ src/read_older2020_matches.py
@@ -1,8 +1,45 @@
+"""Jリーグデータサイトから、各年の全試合データを取得して保存
+"""
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+import re
 
-for _year in range(1994, 2021):
+from read_jleague_matches import store_all_matches
+
+MATCH_CARD_ID = re.compile(r'/SFMS02/\?match_card_id=(\d+)')
+
+def read_href(td_tag) -> str:
+    """取得した試合結果を表すtdタグ内容から、リンク先のhrefを取得し、match_card_idを返す
+    リンクが設定されていない時は、Noneを返す
+    """
+    a_tag = td_tag.find('a')
+    if a_tag:
+        # print(a_tag['href'])
+        return MATCH_CARD_ID.search(a_tag['href'])[1]
+    else:
+        # print(_td)
+        return None
+
+
+def store_year_data(year: int) -> None:
+    """与えられた年のJリーグ試合データを取得し、DataFrame化してファイル保存
+    試合結果列にaタグでリンクが張られていた場合は、試合IDを読み取って列に加える
+    """
     print(_year)
     _text = requests.request('GET', f'https://data.j-league.or.jp/SFMS01/search?competition_years={_year}&tv_relay_station_name=').text
-    pd.read_html(_text)[0].to_csv(f'../csv/{_year}.csv')
+    _df = pd.read_html(_text)[0]
+    soup = BeautifulSoup(_text, 'lxml')
+    id_list = []
+    match_td_list = soup.find_all('td', class_='al-c')
+    for _td in match_td_list:
+        id_list.append(read_href(_td))
+    _df['match_card_id'] = id_list
+    _df.to_csv(f'../csv/{_year}.csv')
 
+
+if __name__ == '__main__':
+    import os
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    for _year in range(1993, 2021):
+        store_year_data(_year)
