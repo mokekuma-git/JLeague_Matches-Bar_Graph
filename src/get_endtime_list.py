cfe11fbf929da73632a2dc39debc5e3d92b55025
mokekuma-git <mokekuma.git@gmail.com>
2022-02-20 22:14:07 +0900

    set updater of game result to workflow / game datetime collector

---
+++ src/get_endtime_list.py
@@ -0,0 +1,28 @@
+from datetime import timedelta
+import pandas as pd
+
+_df = []
+for i in [1, 2, 3]:
+    _df.append(pd.read_csv(f'docs/csv/2022_allmatch_result-J{i}.csv', index_col=0))
+
+time_df = pd.DataFrame()
+for i in [0, 1, 2]:
+    time_df = pd.concat([time_df, _df[i][['match_date', 'start_time']]])
+
+time_df = time_df[time_df['start_time'] != '未定']
+time_df['datetime'] = time_df['match_date'] + ' ' + time_df['start_time']
+time_df['datetime'] = pd.to_datetime(time_df['datetime'])
+
+time_df['datetime'] += timedelta(minutes=135)
+time_df['datetime'] -= timedelta(hours=9)
+
+time_df['m'] = time_df['datetime'].dt.minute
+time_df['h'] = time_df['datetime'].dt.hour
+time_df['D'] = time_df['datetime'].dt.day
+time_df['M'] = time_df['datetime'].dt.month
+time_df['WD'] = '*'
+
+time_df = time_df.sort_values('datetime')
+time_df = time_df.drop_duplicates(subset=['datetime'])
+
+time_df.to_csv('datetimes.csv')
