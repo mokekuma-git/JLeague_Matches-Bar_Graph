45f80bdcc2251c49170ddfb19e55c9b33d11c329
mokekuma-git <mokekuma.git@gmail.com>
2022-05-15 19:13:48 +0900

    Add switching scripts by call

---
+++ scripts/call_update_csv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RUNNING_HOUR=`ZONE="JST" date "+%H"`
+
+echo $RUNNING_HOUR
+
+if [$RUNNING_HOUR -eq 1]; then
+  # 日ごと深夜自動実行 ⇒ 全CSVのアップデートを実行
+  python src/read_jleague_matches.py
+  python src/read_jfamatch.py PrincePremierE
+  python src/read_jfamatch.py PrincePremierW
+  python src/read_jfamatch.py PrinceKanto
+  # python src/read_jfamatch.py WC2022AFC_F
+  # python src/read_we-league.py
+  # python src/read_aclgl_matches.py
+else
+  # 試合時間ごとの自動実行は、現在はJリーグ分のみ
+  python src/read_jleague_matches.py
+fi
