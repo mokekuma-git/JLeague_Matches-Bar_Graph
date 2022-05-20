7c2b1c03861d1b5c2638e61262ecd66d70aff329
mokekuma-git <mokekuma.git@gmail.com>
2022-05-20 09:21:54 +0900

    Set trigger param in call_update_csv.sh

--- scripts/call_update_csv.sh
+++ scripts/call_update_csv.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 echo Called by schedule $1
+DAILY=Daily
+ONGAME=OnGame
+TRIGGER=$(echo $1 | awk '{if($3 == "*" && $4 == "*") {print "'$DAILY'"} else {print "'$ONGAME'"}}')
 
 RUNNING_HOUR=`TZ=Asia/Tokyo date "+%H"`
 TZ=Asia/Tokyo date
 echo $RUNNING_HOUR
+echo $TRIGGER
 
 if [ $RUNNING_HOUR -eq 1 ]; then
+# if [ $TRIGGER = $DAILY]; then
   # 日ごと深夜自動実行 ⇒ 全CSVのアップデートを実行
   python src/read_jleague_matches.py -f
   python src/read_jfamatch.py PrincePremierE
