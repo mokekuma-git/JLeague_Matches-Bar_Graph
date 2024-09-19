ff05d8022b85cdc4ee88bd97b9976a73448ec65d
mokekuma-git <mokekuma.git@gmail.com>
2024-09-19 13:43:46 +0900

    Update call_update_csv.sh to run read jfa match.

--- scripts/call_update_csv.sh
+++ scripts/call_update_csv.sh
@@ -14,11 +14,11 @@ if [ $RUNNING_HOUR -eq 1 ]; then
 # if [ $TRIGGER = $DAILY]; then
   # 日ごと深夜自動実行 ⇒ 全CSVのアップデートを実行
   python src/read_jleague_matches.py -f
-  # python src/read_jfamatch.py PrincePremierE PrincePremierW PrinceKanto
+  python src/read_jfamatch.py PrincePremierE PrincePremierW PrinceKanto
   # python src/read_we-league.py
   # python src/read_aclgl_matches.py
 else
   # 試合時間ごとの自動実行は、現在はJリーグ分のみ
   python src/read_jleague_matches.py
-  python src/read_jfamatch.py PrincePremierE PrincePremierW PrinceKanto
+  # python src/read_jfamatch.py PrincePremierE PrincePremierW PrinceKanto
 fi
